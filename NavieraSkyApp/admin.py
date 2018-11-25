2d78c111aad7bb888105e43e0f8d2e867604acfa
Marco Orellana <orellanas.marco@gmail.com>
2018-11-25 00:30:21 -0600

    Add Contenedores model

--- NavieraSkyApp/admin.py
+++ NavieraSkyApp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from NavieraSkyApp.models import Contenedores
+
 # Register your models here.
+admin.site.register(Contenedores)
--- NavieraSkyApp/models.py
+++ NavieraSkyApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Contenedores(models.Model):
+
+    codigo = models.CharField(max_length=25)
+    bahia = models.CharField(max_length=255)
+    fecha_ingreso = models.DateField()
+
+    def __str__(self):
+        return self.codigo
+
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for this book."""
+        return reverse('detalle-contenedor', args=[str(self.id)])
