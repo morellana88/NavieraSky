e169dc1029941151dadb5b9b9b655dc9d8160a26
Marco Orellana <orellanas.marco@gmail.com>
2018-11-25 00:50:13 -0600

    Add index and basic Contenedores view

--- NavieraSkyApp/views.py
+++ NavieraSkyApp/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from NavieraSkyApp.models import Contenedores
+
+
+def index(request):
+    contenedores = Contenedores.objects.all().count()
+
+    context = {
+        'contenedores': contenedores,
+    }
+
+    return render(request, 'index.html', context=context)
