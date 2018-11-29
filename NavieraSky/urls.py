ceb6a7737a65e3c50dfb0a21b535a8adcd84fc9c
Marco Orellana <orellanas.marco@gmail.com>
2018-11-28 22:15:43 -0600

    Add basic login

--- NavieraSky/urls.py
+++ NavieraSky/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from NavieraSkyApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', views.index, name='index'),
     path('contenedores/', views.ContenedorListView.as_view(), name='contenedores_list'),
     path('contenedores/<int:pk>', views.ContenedorLDetailView.as_view(), name='contenedores_list'),
